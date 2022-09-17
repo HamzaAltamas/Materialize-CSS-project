5434908f0a2c5ca9df308aa7aa63e884197773e7
Hamza Altamas <hamza.altamas.01876@gmail.com>
2022-09-17 23:57:22 +0600

    responsive navbar added with button and form modal...
    You can find a form by clicking in navbar contact button
    and you cannot find any mui default color I changed everything with the help of jquery

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// side nav
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+// nav dropdown trigger
+$(".dropdown-trigger").dropdown();
+
+// modal trigger
+$(document).ready(function () {
+  $(".modal").modal();
+});
+
+// modal placeholder name color change
+$(".name-modal-inputs").click(function () {
+  $(".name-input-name").addClass("input-name-color-change");
+  $(".nameicon").addClass("input-name-color-change");
+});
+
+// modal placeholder telephone color change
+
+$(".tel-modal-inputs").click(function () {
+  $(".tel-input-name").addClass("input-name-color-change");
+  $(".telicon").addClass("input-name-color-change");
+});
+
+// modal placeholder email color change
+
+$(".mail-modal-inputs").click(function () {
+  $(".mail-input-name").addClass("input-name-color-change");
+  $(".mailicon").addClass("input-name-color-change");
+});
+
+// modal placeholder message color change
+
+$(".msg-modal-inputs").click(function () {
+  $(".msg-input-name").addClass("input-name-color-change");
+  $(".msgicon").addClass("input-name-color-change");
+});
